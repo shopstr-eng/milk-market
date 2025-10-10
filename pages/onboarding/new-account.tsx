c2f7ede9dc5526e505a4172297c092643941be1a
calvadev⚡️ <32919103+calvadev@users.noreply.github.com>
2025-10-10 05:32:03 +0000

    Build fix

--- pages/onboarding/new-account.tsx
+++ pages/onboarding/new-account.tsx
@@ -123,7 +123,7 @@ const Keys = () => {
                   <Tooltip
                     content="This passphrase acts as a password and is used to keep your account secure. Remember it and keep it safe as it can't be recovered!"
                     placement="bottom"
-                    trigger="click"
+                    trigger="focus"
                     classNames={{
                       content: "bg-dark-bg text-dark-text p-2 max-w-xs",
                     }}
